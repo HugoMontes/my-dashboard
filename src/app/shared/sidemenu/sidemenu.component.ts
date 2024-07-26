8f612c147caf8bb2acd71ea345813e34fcfdad24
hmontes <hugomontes927@gmail.com>
2024-07-26 15:32:48 -0400

    7. Configuración del dashboard | Tailwind

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
@@ -9,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class SidemenuComponent {
 
+  public menuItems = routes
+    // Con .map barremos todas las rutas de app.routes.
+    // con '?? []' indicamos que si no existe children, retorne un array vacio.
+    .map(route => route.children ?? [])
+    // Combina todos los subarreglos en un solo arreglo
+    .flat()
+    // Filtrar unicamente a los elementos que tengan valor en path
+    .filter(route => route && route.path)
+    // Filtrar aquel elemento que en su path tenga ':'
+    .filter(route => !route.path?.includes(':'));
+
+  constructor() {
+    console.log(this.menuItems);
+  }
 }
