c57f13e37c38d476f0c93002a04cacfacf437471
hmontes <hugomontes927@gmail.com>
2024-07-26 19:42:32 -0400

    11. Control Flow | @Switch

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 
+// Crear un tipo de dato que permite unicamente 3 valores
+type Grade = 'A'|'B'|'F';
+
 @Component({
   standalone: true,
   imports: [],
@@ -10,6 +13,9 @@ export default class ControlFlowComponent {
 
   // Crear una señal como propiedad de clase con valor por defecto false
   public showContent = signal(false);
+
+  // Crear una nueva señal de tipo Grade
+  public grade = signal<Grade>('A');
 
   // Crear un metodo que cambia el valor de la señal
   public toggleContent() {
