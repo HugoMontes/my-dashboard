3225866ca5f9576cc51a5309c7aaed5ea78cfd25
hmontes <hugomontes927@gmail.com>
2024-07-29 17:19:44 -0400

    19. ViewTransitions API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -41,9 +41,14 @@ export const routes: Routes = [
                 loadComponent: () => import('./dashboard/pages/defer-views/defer-views.component'),
             },
             {
-                path: 'view-transition',
-                title: 'View Transition',
-                loadComponent: () => import('./dashboard/pages/view-transition/view-transition.component'),
+                path: 'view-transition-1',
+                title: 'View Transition 1',
+                loadComponent: () => import('./dashboard/pages/view-transition/view-transition1.component'),
+            },
+            {
+                path: 'view-transition-2',
+                title: 'View Transition 2',
+                loadComponent: () => import('./dashboard/pages/view-transition/view-transition2.component'),
             },
             // Añadimos un comodin para cuando se ingrese en la url /dashboard
             // redireccionamos a control-flow
---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="View Transition"/>
+
+    <section class="flex justify-start">
+
+      <img srcset="http://picsum.photos/id/237/200/300" 
+          alt="Picsum" width="200" height="300"/>
+
+      <div class="bg-blue-500 w-56 h-56"></div>
+
+    </section>
+  `,
+})
+export default class ViewTransitionComponent {
+
+}
