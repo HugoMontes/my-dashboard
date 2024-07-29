fc26f55402702d39ce033df25ca8207b5b03021f
hmontes <hugomontes927@gmail.com>
2024-07-29 11:06:36 -0400

    17. Defer Triggers | Interacciones

--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <h1>Hola Mundo</h1>
+   <section [ngClass]="['w-full', cssClass]">
+    <!-- Insertar contenido dinamico en la plantila desde el exterior -->
+    <ng-content></ng-content>
+   </section>
   `,
 })
 export class HeavyLoadersFastComponent {
+  // Recibir cssClass requerido
+  // Indicar que siempre debe tener un valor "cssClass!"
+  @Input({ required: true }) cssClass!: string;
 
+  constructor() {
+    console.log('HeavyLoaderFast creado.');
+  }
 }
