2a3148623cbd45e7345e4deddaa06820b0ca6b0f
hmontes <hugomontes927@gmail.com>
2024-07-29 18:30:25 -0400

    20. Hero Animation | View Transitions

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -6,14 +6,17 @@ import { TitleComponent } from '@shared/title/title.component';
   standalone: true,
   imports: [CommonModule, TitleComponent],
   template: `
-    <app-title title="View Transition"/>
+    <app-title title="View Transition 1"/>
 
     <section class="flex justify-start">
 
       <img srcset="http://picsum.photos/id/237/200/300" 
-          alt="Picsum" width="200" height="300"/>
+          alt="Picsum" width="200" height="300"
+          style="view-transition-name: hero1"/>
 
-      <div class="bg-blue-500 w-56 h-56"></div>
+      <div class="bg-blue-500 w-56 h-56"
+           style="view-transition-name: hero2">
+      </div>
 
     </section>
   `,
