c25e8720e50fa0d7c852f722c632a16c9db26226
hmontes <hugomontes927@gmail.com>
2024-07-30 19:16:16 -0400

    23. De observable a Señal | toSignal

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TitleComponent } from "../../../shared/title/title.component";
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './user.component.html',
   styles: ``
 })
-export default class DeferUserComponent {
+export default class UserComponent {
 
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
+
+  public user = toSignal(
+    this.route.params.pipe(
+      // Con switchMap destructuramos id y lo mandamos a getUserById
+      switchMap(({ id }) => this.usersService.getUserById(id))
+    )
+  )
 }
