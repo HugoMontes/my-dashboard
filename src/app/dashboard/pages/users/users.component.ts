3043d2e539b9034170914b20ccccb20c3895f9d7
hmontes <hugomontes927@gmail.com>
2024-07-30 18:10:12 -0400

    22. Peticiones HTTP | importProvidersFrom

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
+import { TitleComponent } from "../../../shared/title/title.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent, RouterModule],
   templateUrl: './users.component.html',
   styles: ``
 })
 export default class UsersComponent {
 
-  // Agregar un propiedad injectando UsersService
-  public usersService = inject(UsersService)
+  // Agregar un propiedad inyectando UsersService
+  public usersService = inject(UsersService);
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, signal } from '@angular/core';
-import { User } from '../interfaces/req-response';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { User, UsersResponse } from '../interfaces/req-response';
+import { HttpClient } from '@angular/common/http';
+import { delay } from 'rxjs';
 
 // Crear una interface
 interface State {
@@ -12,13 +14,33 @@ interface State {
 })
 export class UsersService {
 
+  // Instanciar el sevicio HttpClient de Angular
+  private http = inject(HttpClient);
+
   // Crear una propiedad privado de tipo señal 
   #state = signal<State>({
     loading: true,
     users: [],
-  })
+  });
+
+  // Usar señales computadas para devolver los usuarios desde una propiedad privada
+  // Las señales seran usandas unicamente en modo lectura.
+  public users = computed(() => this.#state().users);
+  public loading = computed(() => this.#state().loading);
 
   constructor() {
-    console.log('Cargando data...');
+    // Disparar la peticion HTTP
+    //this.http.get<UsersResponse>('/api/users')
+    this.http.get<UsersResponse>('https://reqres.in/api/users')
+      // Retrasamos la emision de la respuesta por 1.5 segundos
+      .pipe(delay(1500))
+      // Suscribir al observable devuelto por http.get
+      .subscribe(res => {      
+        // Establecer la informacion en la señal
+        this.#state.set({
+          loading: false,
+          users: res.data,
+        });
+      })
   }
 }
